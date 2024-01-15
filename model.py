d059898e80db590cbd25c2874dd8bec128008e5c
LIU42 <3528865430@qq.com>
2024-01-15 16:02:06 +0800

    少量修改。

--- model.py
+++ model.py
@@ -3,10 +3,7 @@ import torch
 from torch import nn
 from torchvision import models
 
-def load_pretrain_alexnet(classes_count) -> None:
-    model = models.alexnet(weights = models.AlexNet_Weights.DEFAULT)
-    model.classifier[6] = nn.Linear(4096, classes_count)
-    torch.save(model, "./weights/AlexNet-Pretrain.pt")
+model = models.alexnet(weights = models.AlexNet_Weights.DEFAULT)
+model.classifier[6] = nn.Linear(4096, 10)
 
-if __name__ == "__main__":
-    load_pretrain_alexnet(classes_count = 10)
+torch.save(model, "./weights/AlexNet-Pretrain.pt")
