70f2fd1bdadc1681f5ea6e60e41586d1e691a1c3
LIU42 <3528865430@qq.com>
2025-06-20 17:37:12 +0800

    少量修改。

--- servers/server.py
+++ servers/server.py
@@ -16,10 +16,13 @@ def decode_stream(stream):
 
 
 def center_crop(image, size=224):
-    current_size = min(image.shape[0], image.shape[1])
+    w = image.shape[1]
+    h = image.shape[0]
 
-    x1 = (image.shape[1] - current_size) >> 1
-    y1 = (image.shape[0] - current_size) >> 1
+    current_size = min(w, h)
+
+    x1 = (w - current_size) >> 1
+    y1 = (h - current_size) >> 1
 
     x2 = x1 + current_size
     y2 = y1 + current_size
@@ -31,28 +34,29 @@ def normalize(inputs):
     return (inputs - 127.5) / 127.5
 
 
-def convert_inputs(image):
-    converted_inputs = cv2.cvtColor(center_crop(image), cv2.COLOR_BGR2RGB).transpose((2, 0, 1))
-    converted_inputs = normalize(converted_inputs)
+def preprocess(image):
+    inputs = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).transpose((2, 0, 1))
 
     if configs['precision'] == 'fp16':
-        return np.expand_dims(converted_inputs, axis=0).astype(np.float16)
+        inputs = normalize(inputs).astype(np.float16)
     else:
-        return np.expand_dims(converted_inputs, axis=0).astype(np.float32)
+        inputs = normalize(inputs).astype(np.float32)
 
+    return np.expand_dims(inputs, axis=0)
+    
 
 def softmax(inputs):
     return np.exp(inputs) / np.sum(np.exp(inputs), axis=0)
 
 
 def inference(image):
-    classes_outputs = session.run(['outputs'], {'inputs': convert_inputs(image)})
-    classes_outputs = classes_outputs[0]
+    predictions = session.run(['outputs'], {'inputs': preprocess(image)})
+    predictions = predictions[0]
 
-    return outputs_postprocess(classes_outputs.squeeze())
+    return output_postprocess(predictions.squeeze())
 
 
-def outputs_postprocess(outputs):
+def output_postprocess(outputs):
     index = np.argmax(outputs)
     confidences = softmax(outputs)
 
