df0d76e39217ca5453026ea79859dbaaa7a2b244
LIU42 <3528865430@qq.com>
2025-07-28 15:49:24 +0800

    少量修改。

--- servers/server.py
+++ servers/server.py
@@ -11,18 +11,23 @@ app = flask.Flask(__name__)
 session = ort.InferenceSession(configs['model-path'], providers=configs['providers'])
 
 
+def softmax(inputs):
+    return np.exp(inputs) / np.sum(np.exp(inputs), axis=0)
+
+
+def normalize(inputs):
+    return (inputs - 127.5) / 127.5
+
+
 def decode_stream(stream):
     return cv2.imdecode(np.frombuffer(stream.read(), dtype=np.uint8), cv2.IMREAD_UNCHANGED)
 
 
 def center_crop(image, size=224):
-    w = image.shape[1]
-    h = image.shape[0]
+    current_size = min(image.shape[1], image.shape[0])
 
-    current_size = min(w, h)
-
-    x1 = (w - current_size) >> 1
-    y1 = (h - current_size) >> 1
+    x1 = (image.shape[1] - current_size) >> 1
+    y1 = (image.shape[0] - current_size) >> 1
 
     x2 = x1 + current_size
     y2 = y1 + current_size
@@ -30,12 +35,8 @@ def center_crop(image, size=224):
     return cv2.resize(image[y1:y2, x1:x2], (size, size), interpolation=cv2.INTER_LINEAR)
 
 
-def normalize(inputs):
-    return (inputs - 127.5) / 127.5
-
-
 def preprocess(image):
-    inputs = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).transpose((2, 0, 1))
+    inputs = cv2.cvtColor(center_crop(image), cv2.COLOR_BGR2RGB).transpose((2, 0, 1))
 
     if configs['precision'] == 'fp16':
         inputs = normalize(inputs).astype(np.float16)
@@ -43,10 +44,6 @@ def preprocess(image):
         inputs = normalize(inputs).astype(np.float32)
 
     return np.expand_dims(inputs, axis=0)
-    
-
-def softmax(inputs):
-    return np.exp(inputs) / np.sum(np.exp(inputs), axis=0)
 
 
 def inference(image):
@@ -58,20 +55,20 @@ def inference(image):
 
 def output_postprocess(outputs):
     index = np.argmax(outputs)
-    confidences = softmax(outputs)
+    scores = softmax(outputs)
 
-    return index.item(), confidences[index].item()
+    return index.item(), round(scores[index].item(), 3)
 
 
-def classify(stream):
-    return inference(decode_stream(stream))
+def decode_and_inference(stream):
+    index, score = inference(decode_stream(stream))
+
+    return {
+        'index': index,
+        'score': score,
+    }
 
 
 @app.post('/flowerclassify')
 def flower_classify():
-    try:
-        index, confidence = classify(flask.request.files['image'].stream)
-    except KeyError:
-        flask.abort(400)
-
-    return flask.jsonify({'index': index, 'confidence': confidence})
+    return flask.jsonify(decode_and_inference(flask.request.files['image'].stream))
